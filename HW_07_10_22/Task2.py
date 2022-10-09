00f857c183e8f5530abb2a0e48848b8ac146cbdf
GalinaS <peraskevasokolova@gmail.com>
2022-10-09 18:05:48 +0400

    truth of the statement, added counter

--- HW_07_10_22/Task2.py
+++ HW_07_10_22/Task2.py
@@ -1,13 +1,26 @@
 # Напишите программу для. проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+from itertools import count
+from unittest import result
+
+
 print('Левая часть выражения ¬(X ⋁ Y ⋁ Z)   Правая часть выражения ¬X ⋀ ¬Y ⋀ ¬Z\n')
 print(' X \tY \tZ \tРезультат   X\t  Y\t  Z\tРезультат\n')
+count=0
 for x in [0, 1]:
     for y in [0, 1]:
         for z in [0, 1]:
-            print(f'{x}\t{y}\t{z}\t  {not(x or y or z)} |    {x}\t   {y}\t {z}\t{not x and not y and not z}')
+            resultLeft = not(x or y or z)
+            resultRight = not x and not y and not z
+            print(f'{x}\t{y}\t{z}\t  {resultLeft} |    {x}\t   {y}\t {z}\t{resultRight}')
+            if resultLeft == resultRight:
+                count+=1
+if count == 8:
+    print('Результаты идентичны, утверждение истинно') 
+else:
+    print('Утверждение ложно')    
 
-print('Результаты идентичны, утверждение истинно')            
+            
     
 
 

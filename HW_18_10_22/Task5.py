9f0f80a9dbb7ee35b95982c686b03dfa8ef4b5a9
GalinaS <peraskevasokolova@gmail.com>
2022-10-25 22:52:02 +0400

    addition of polynomials

--- HW_18_10_22/Task5.py
+++ HW_18_10_22/Task5.py
@@ -12,7 +12,7 @@ import random
 
 def polynomial_notation(K):
     coefficient=[random.randint(-10, 10)]
-    for i in range(1, k1+1):
+    for i in range(1, K+1):
         coefficient.append(random.randint(-10, 10))
     polindrom = ''
     for i in range(K, 0, -1):
@@ -48,21 +48,19 @@ polynom2.close()
 
 f1 = open('pol1.txt', 'r')
 pol1_lst=f1.readline()
-pol1_lst2=pol1_lst.replace(" ", "").replace("-", "+-")[:-2].split("+")
-print(pol1_lst)
-print(pol1_lst2)
-exit()
+pol1_lst2=pol1_lst.replace(" ", "").replace("-", "+-")[:-2]
+if pol1_lst2[0]=='+':
+    pol1_lst2=pol1_lst2[1:]
+pol1_lst2=pol1_lst2.split("+")    
+#print(pol1_lst2)
+
 coef1={}
 if 'x' not in pol1_lst2[-1]:
-    #coef1[0]=int(pol1_lst2[-1])
-    #del pol1_lst2[-1]
     pol1_lst2[-1]+='x^0'
-    #print(pol1_lst2[-1])
 
 for i in range(len(pol1_lst2)):
     if '^' not in pol1_lst2[i]:
         pol1_lst2[i]+='^1'
-        print(pol1_lst2[i])
 
 for i in pol1_lst2:
     a, b = i.split('x')
@@ -71,28 +69,25 @@ for i in pol1_lst2:
     elif a == '-':
         a=-1
     coef1[int(b[-1])]=int(a)            
-print(pol1_lst2)
-print(coef1)
+# print(pol1_lst2)
+# print(coef1)
 f1.close()
 
 f2 = open('pol2.txt', 'r')
 pol2_lst=f2.readline()
-pol2_lst2=pol2_lst.replace(" ", "").replace("-", "+-")[:-2].split("+")
-print(pol2_lst)
-print(pol2_lst2)
+pol2_lst2=pol2_lst.replace(" ", "").replace("-", "+-")[:-2]
+if pol2_lst2[0]=='+':
+    pol2_lst2=pol2_lst2[1:]
+pol2_lst2=pol2_lst2.split("+")
 
 coef2={}
 if 'x' not in pol2_lst2[-1]:
-    #coef1[0]=int(pol1_lst2[-1])
-    #del pol1_lst2[-1]
     pol2_lst2[-1]+='x^0'
-    #print(pol1_lst2[-1])
-
+    
 for i in range(len(pol2_lst2)):
     if '^' not in pol2_lst2[i]:
         pol2_lst2[i]+='^1'
-        print(pol2_lst2[i])
-
+        
 for i in pol2_lst2:
     a1, b1 = i.split('x')
     if a1 == '':
@@ -100,6 +95,35 @@ for i in pol2_lst2:
     elif a1 == '-':
         a1=-1
     coef2[int(b1[-1])]=int(a1)            
-print(pol2_lst2)
-print(coef2)
+#print(pol2_lst2)
+#print(coef2)
 f2.close()
+
+max_coef=max(max(coef1.keys()), max(coef2.keys()))
+coef={}
+for i in range(max_coef+1):
+    coef[i]=coef1.get(i, 0)+coef2.get(i, 0)
+#print(coef) 
+valLst=list(coef.values()) 
+#print(valLst)  
+
+res = ''
+for i in range(len(valLst)-1, 0, -1):
+    if valLst[i] == 1:
+        res+='+x^'+str(i)
+    elif valLst[i] == -1:  
+        res+='-x^'+str(i) 
+    elif valLst[i] == 0:
+        res+=''
+    else:
+        res+='+'+str(valLst[i])+'x^'+str(i)   
+if valLst[0] != 0:        
+    res+='+'+str(valLst[0])  
+res = res.replace("^1+", "+").replace("+-", "-")+'=0'  
+if res[0] == '+':
+    res = res[1:]
+print('Результат сложения 2-х многочленов (записан в файл poly.txt): \n', res) 
+
+poly=open('poly.txt', 'w')
+poly.writelines(res)
+poly.close()       
